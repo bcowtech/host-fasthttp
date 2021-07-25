382f372b072828ea2a93e5fbed8d80d674a71013
Wilhelm Liao <williaml.twcp@gmail.com>
2021-07-25 23:41:09 +0800

    WIP: fix struct symbol

--- internal/fasthttpHostService.go
+++ internal/fasthttpHostService.go
@@ -10,18 +10,18 @@ var _ host.HostService = new(FasthttpHostService)
 
 type FasthttpHostService struct{}
 
-func (p *FasthttpHostService) Init(h host.Host, app *host.AppContext) {
+func (s *FasthttpHostService) Init(h host.Host, app *host.AppContext) {
 	if v, ok := h.(*FasthttpHost); ok {
 		v.preInit()
 	}
 }
 
-func (p *FasthttpHostService) InitComplete(h host.Host, app *host.AppContext) {
+func (s *FasthttpHostService) InitComplete(h host.Host, app *host.AppContext) {
 	if v, ok := h.(*FasthttpHost); ok {
 		v.init()
 	}
 }
 
-func (p *FasthttpHostService) GetHostType() reflect.Type {
+func (s *FasthttpHostService) GetHostType() reflect.Type {
 	return typeOfHost
 }
